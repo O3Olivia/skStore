78bf53f3bc555909271c73dcd680cb286b58bfd0
O3Olivia <cherishtmwu@gmail.com>
2023-03-02 16:20:49 +0900

    update: 에러 수정

--- src/pages/board/boardPage.js
+++ src/pages/board/boardPage.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 import BoardList from "../../components/board/BoardList";
-import UseHttp from "../../hooks/";
+import UseHttp from "../../hooks/use-http";
 import { getAllBoards } from "../../store/board-api";
 
 import NoBoardFound from "../../components/board/NoBoardFound";
